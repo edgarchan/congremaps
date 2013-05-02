b4cb41c9daa1d3b3f3f8cadfa5bc90081bc45ddd
Edgar Chan <edgarchancarrillo@gmail.com>
2013-05-02 16:20:34 -0500

    Agregamos la lista de marcadores y la funcion para ir hacia el marcador

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,8 +1,40 @@
 var congremapsApp = angular.module('congremapsApp', ['ui.bootstrap', 'ui']);
+congremapsApp.factory('Markers', function(){
+   return { lista: new Array(),
+            info: new Array(),
+            goTo: function(idx){}
+         }
+});
 
-function MapCtrl($scope, $http) {
+function MuestraMsg(){
+ var el = $("#msg-status");
+ if(!el.is(":visible")){
+     $("#msg-status").fadeIn();
+     $("#msg-status").fadeOut(1200);
+ }
+}
 
-	$scope.myMarkers = [];
+function MarcadorCtrl($scope, Markers) {
+    $scope.myMarkers  = Markers.lista;
+
+    $scope.mkrLabel = function(m){
+       return "Entidad: " +Markers.info[m].entidad +  " Distrito: " +Markers.info[m].distrito;
+    }
+
+    $scope.mkrInfo = function(m){
+       Markers.goTo(m);
+    }
+
+}
+
+function MapCtrl($scope, $http, Markers) {
+
+	$scope.myMarkers = Markers.lista;
+
+	Markers.goTo = function(idx){
+	    var mkr = $scope.myMarkers[idx];
+	    $scope.myMap.panTo(mkr.getPosition());
+	}
 
 	$scope.mapOptions = {
 		center: new google.maps.LatLng(21.782, -102.137),
@@ -11,20 +43,26 @@ function MapCtrl($scope, $http) {
 	};
 
 	$scope.addMarker = function($event) {
-	    var mkr = new google.maps.Marker({
-                  			map: $scope.myMap,
-                  			position: $event.latLng
-                  });
-		$scope.myMarkers.push(mkr);
+		$http.get(
+		  'loc/'+ $event.latLng.kb + '/' + $event.latLng.jb
+		).success(function(info) {
+            var mkr = new google.maps.Marker({
+                                map: $scope.myMap,
+                                position: $event.latLng
+                      });
+            $scope.myMarkers.push(mkr);
+            Markers.info.push(info);
+
+		}).error(function(noinf){
+		   MuestraMsg();
+		});
 	};
 
-	$scope.openMarkerInfo = function(marker) {
-      $http.get('loc/'+ marker.getPosition().lng() + '/' + marker.getPosition().lat() ).success(function(info) {
-		$scope.currentMarker = marker;
+	$scope.openMarkerInfo = function(idx) {
+	    var info = Markers.info[idx];
+		$scope.currentMarker = $scope.myMarkers[idx];
 		$scope.entidad = info.entidad;
 		$scope.distrito = info.distrito;
-		$scope.myInfoWindow.open($scope.myMap, marker);
-      });
+		$scope.myInfoWindow.open($scope.myMap, $scope.currentMarker);
 	};
-
 }
